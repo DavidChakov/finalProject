88eb6a4ab534e42a6e510d8797c1fafa2bd098bf
DavidChakov <77823382+DavidChakov@users.noreply.github.com>
2025-11-24 20:04:33 -0500

    adding new enemy type

--- final-project/Scripts/enemy.gd
+++ final-project/Scripts/enemy.gd
@@ -1,18 +1,21 @@
 extends Node2D
 
 # Loads bullet scene into var bullet_scene
-var bullet_scene = load("res://Scenes/bullet.tscn")
+var bullet_scene = load("res://Scenes/bullet_default.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Calls timeout() function every X seconds
-	$Timer.set_wait_time(.1)
+	$Timer.set_wait_time(.2)
 	$Timer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# Rotates the enemy
-	rotate(1)
+	rotate(1 * delta)
+	
+	# Moves the enemy
+	self.position.x += .2
 
 # Spawns bullets
 func spawn_bullets():
--- final-project/Scripts/player.gd
+++ final-project/Scripts/player.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	# Has player follow mouse
 	self.position = get_global_mouse_position()
 	pass
